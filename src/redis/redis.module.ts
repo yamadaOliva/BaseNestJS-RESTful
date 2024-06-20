a4989837cff1e890032524cf2eedc9effd00d298
yamadaOliva <vietanh87.forwork@gmail.com>
2024-06-21 00:19:57 +0700

    hehe

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -2,13 +2,17 @@ import { Module, Global } from '@nestjs/common';
 import { Redis } from 'ioredis';
 import * as dotenv from 'dotenv';
 dotenv.config();
+console.log('hehe', process.env.REDIS_BASE, parseInt(process.env.REDIS_HOST));
 const redisProvider = {
   provide: 'REDIS_CLIENT',
   useFactory: () => {
-    const redis = new Redis({
-      host: process.env.REDIS_BASE || 'localhost',
-      port: parseInt(process.env.REDIS_HOST) || 6379,
-    });
+    const redisUrl = process.env.REDIS_URL || null;
+    const redis = redisUrl
+      ? new Redis(redisUrl)
+      : new Redis({
+          host: process.env.REDIS_BASE || 'localhost',
+          port: parseInt(process.env.REDIS_HOST) || 6379,
+        });
     return redis;
   },
 };
